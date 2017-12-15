9855899c844a53bdbf715e0f98d5d121da205ec7
cryptoprojects <nokes2000@yahoo.de>
2017-12-15 07:28:04 +0100

    Revert "tried other values this should fix a bug"
    This reverts commit 2f05754a6928ce0cc9f3b9d4e0a6fa8d29b6d26d.

--- src/validation.h
+++ src/validation.h
@@ -119,9 +119,9 @@ static const unsigned int AVG_FEEFILTER_BROADCAST_INTERVAL = 10 * 60;
 /** Maximum feefilter broadcast delay after significant change. */
 static const unsigned int MAX_FEEFILTER_CHANGE_DELAY = 5 * 60;
 /** Block download timeout base, expressed in millionths of the block interval (i.e. 30 seconds) */
-static const int64_t BLOCK_DOWNLOAD_TIMEOUT_BASE = 30000;
+static const int64_t BLOCK_DOWNLOAD_TIMEOUT_BASE = 300000;
 /** Additional block download timeout per parallel downloading peer (i.e. 15 seconds) */
-static const int64_t BLOCK_DOWNLOAD_TIMEOUT_PER_PEER = 15000;
+static const int64_t BLOCK_DOWNLOAD_TIMEOUT_PER_PEER = 150000;
 
 static const int64_t DEFAULT_MAX_TIP_AGE = 24 * 60 * 60;
 /** Maximum age of our tip in seconds for us to be considered current for fee estimation */
